b79154e2854632e4ffe67d51e57c94b4100d2422
samwhelp <samwhelp@users.noreply.github.com>
2025-03-18 19:36:06 +0800

    fix

--- project/gen/hotkey-tips/share/locale/en_US/db/list.php
+++ project/gen/hotkey-tips/share/locale/en_US/db/list.php
@@ -6,7 +6,7 @@
 
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+o";
+	$item['Keybind'] = "Alt + Shift + o";
 	$item['Action'] = "Open the cheatsheet";
 	$list[] = $item;
 
@@ -21,157 +21,157 @@
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+c";
+	$item['Keybind'] = "Alt + Shift + c";
 	$item['Action'] = "i3 Restart";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Ctrl+c";
+	$item['Keybind'] = "Alt + Ctrl + c";
 	$item['Action'] = "i3 Reload";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+F1";
+	$item['Keybind'] = "Alt + F1";
 	$item['Action'] = "Main Menu";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+F2";
+	$item['Keybind'] = "Alt + F2";
 	$item['Action'] = "Runner";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+F3";
+	$item['Keybind'] = "Alt + F3";
 	$item['Action'] = "Window List";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+d";
+	$item['Keybind'] = "Alt + Shift + d";
 	$item['Action'] = "Rofi Show Drun";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+r";
+	$item['Keybind'] = "Alt + Shift + r";
 	$item['Action'] = "Rofi Show Run";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+w";
+	$item['Keybind'] = "Alt + Shift + w";
 	$item['Action'] = "Rofi Show Window";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+f";
+	$item['Keybind'] = "Alt + Shift + f";
 	$item['Action'] = "Launch File Manager (thunar)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+g";
+	$item['Keybind'] = "Alt + Shift + g";
 	$item['Action'] = "Launch File Manager (pcmanfm-qt)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+e";
+	$item['Keybind'] = "Alt + Shift + e";
 	$item['Action'] = "Launch Text Editor (geany)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+b";
+	$item['Keybind'] = "Alt + Shift + b";
 	$item['Action'] = "Launch Web Browser (firedragon)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+s";
+	$item['Keybind'] = "Alt + Shift + s";
 	$item['Action'] = "Launch Appearance Setting (lxappearance)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Ctrl+s";
+	$item['Keybind'] = "Alt + Ctrl + s";
 	$item['Action'] = "Launch Display Setting (lxrandr)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Enter";
+	$item['Keybind'] = "Alt + Enter";
 	$item['Action'] = "Launch Terminal (xfce4-terminal)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+a";
+	$item['Keybind'] = "Alt + Shift + a";
 	$item['Action'] = "Launch Terminal (xfce4-terminal)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Ctrl+a";
+	$item['Keybind'] = "Alt + Ctrl + a";
 	$item['Action'] = "Launch Terminal (sakur)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+t";
+	$item['Keybind'] = "Alt + Shift + t";
 	$item['Action'] = "Launch Terminal (qterminal)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Ctrl+t";
+	$item['Keybind'] = "Alt + Ctrl + t";
 	$item['Action'] = "Launch Terminal (kitty)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+q";
+	$item['Keybind'] = "Win + q";
 	$item['Action'] = "Window Close";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+f";
+	$item['Keybind'] = "Win + f";
 	$item['Action'] = "Window Fullscreen";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+Esc";
+	$item['Keybind'] = "Win + Esc";
 	$item['Action'] = "Window Toggle Float";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+grave";
+	$item['Keybind'] = "Win + grave";
 	$item['Action'] = "Change focus between tiling / floating windows";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+Mouse Left Drag";
+	$item['Keybind'] = "Win + Mouse Left Drag";
 	$item['Action'] = "Window Move (on Floating)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+Mouse Right Drag";
+	$item['Keybind'] = "Win + Mouse Right Drag";
 	$item['Action'] = "Window Resize (on Floating)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+a";
+	$item['Keybind'] = "Win + a";
 	$item['Action'] = "Window Switch Prev";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+s";
+	$item['Keybind'] = "Win + s";
 	$item['Action'] = "Window Switch Next";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+a";
+	$item['Keybind'] = "Alt + a";
 	$item['Action'] = "Workspace Switch Prev";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+s";
+	$item['Keybind'] = "Alt + s";
 	$item['Action'] = "Workspace Switch Next";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+1-5";
+	$item['Keybind'] = "Alt + 1-5";
 	$item['Action'] = "Workspace Switch to 1-5";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+1-5";
+	$item['Keybind'] = "Win + 1-5";
 	$item['Action'] = "Window Send to 1-5";
 	$list[] = $item;
 
@@ -181,77 +181,77 @@
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Win+Print";
+	$item['Keybind'] = "Win + Print";
 	$item['Action'] = "Screenshot Current Window";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Ctrl+Print";
+	$item['Keybind'] = "Ctrl + Print";
 	$item['Action'] = "Screenshot Selected Area";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Fn+F5(XF86MonBrightnessDown)";
+	$item['Keybind'] = "Fn + F5 (XF86MonBrightnessDown)";
 	$item['Action'] = "Brightness Decrease";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Fn+F6(XF86MonBrightnessUp)";
+	$item['Keybind'] = "Fn + F6 (XF86MonBrightnessUp)";
 	$item['Action'] = "Brightness Increase";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+[";
+	$item['Keybind'] = "Alt + Shift + [";
 	$item['Action'] = "Brightness Decrease";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+]";
+	$item['Keybind'] = "Alt + Shift + ]";
 	$item['Action'] = "Brightness Increase";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+v";
+	$item['Keybind'] = "Alt + Shift + v";
 	$item['Action'] = "Launch Volume Control (pavucontrol)";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Fn+F10(XF86AudioMute)";
+	$item['Keybind'] = "Fn + F10 (XF86AudioMute)";
 	$item['Action'] = "Volume Toggle Mute";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Fn+F11(XF86AudioLowerVolume)";
+	$item['Keybind'] = "Fn + F11 (XF86AudioLowerVolume)";
 	$item['Action'] = "Volume Decrease";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Fn+F12(XF86AudioRaiseVolume)";
+	$item['Keybind'] = "Fn + F12 (XF86AudioRaiseVolume)";
 	$item['Action'] = "Volume Increase";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+m";
+	$item['Keybind'] = "Alt + m";
 	$item['Action'] = "Volume Toggle Mute";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+,";
+	$item['Keybind'] = "Alt + Shift + ,";
 	$item['Action'] = "Volume Decrease";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Shift+.";
+	$item['Keybind'] = "Alt + Shift + .";
 	$item['Action'] = "Volume Increase";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Ctrl+,";
+	$item['Keybind'] = "Alt + Ctrl + ,";
 	$item['Action'] = "Volume Decrease Slowly";
 	$list[] = $item;
 
 	$item = array();
-	$item['Keybind'] = "Alt+Ctrl+.";
+	$item['Keybind'] = "Alt + Ctrl + .";
 	$item['Action'] = "Volume Increase Slowly";
 	$list[] = $item;
 
