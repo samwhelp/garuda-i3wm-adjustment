1930c4bbd5cbec4e98016efef1d21a5a440695d6
samwhelp <samwhelp@users.noreply.github.com>
2025-03-18 19:32:26 +0800

    fix

---
+++ project/gen/hotkey-tips/gen-tsv.php
@@ -0,0 +1,101 @@
+#!/usr/bin/env php
+<?php
+
+	require_once(__DIR__ . '/lib/init.php');
+
+class App {
+
+	protected $_Lang = 'en_US';
+
+	protected $_List = array();
+
+	protected $_Color_1 = '#cccccc';
+	protected $_Color_2 = '#ffffff';
+
+	protected $_Font = 'comfortaa 16';
+
+	protected function is_even ($val) {
+
+		$val = intval($val);
+
+		if ($val % 2 === 1) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public function createList ()
+	{
+		$rtn = '';
+
+		foreach ($this->_List as $key => $item) {
+
+			$rtn .= '"';
+			$rtn .= $item['Keybind'];
+			$rtn .= '"';
+			$rtn .= "\t";
+			$rtn .= '"';
+			$rtn .= $item['Action'];
+			$rtn .= '"';
+			$rtn .= PHP_EOL;
+
+		}
+
+		return $rtn;
+	}
+
+	public function run ()
+	{
+		$this->preRun();
+		$this->doRun();
+		$this->postRun();
+	}
+
+	protected function preRun ()
+	{
+		$this->_List = include(__DIR__ . '/share/locale/' . $this->_Lang . '/db/list.php');
+
+		return;
+	}
+
+	protected function postRun ()
+	{
+		return;
+	}
+
+	protected function doRun ()
+	{
+
+		$tmp_dir_path = __DIR__ . '/tmp';
+
+		$target_file_path = $tmp_dir_path . '/hotkey-tips.tsv';
+
+		if (!file_exists($tmp_dir_path)) {
+			if (!mkdir($tmp_dir_path, 0777, true)) {
+				die('Failed to create directories...');
+			}
+		}
+
+
+
+		$template_file_path = __DIR__ . '/share/locale/' . $this->_Lang . '/view/gen-tsv.view.php';
+
+		$data = array();
+		$data['List'] = $this->createList();
+		$data['Shebang'] = '#!/usr/bin/env bash';
+
+		$view = new \Demo\ViewUtil;
+
+		$content = $view->renderStr($template_file_path, $data);
+
+		file_put_contents($target_file_path, $content);
+
+		chmod($target_file_path, 0755);
+
+	}
+
+}
+
+
+	(new App)->run();
